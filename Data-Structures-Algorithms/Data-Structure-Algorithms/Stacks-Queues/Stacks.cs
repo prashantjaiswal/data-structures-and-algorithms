f98c3fe7de9b9f177259e5f74d107b09475cc4ee
Prashant Jaiswal <jaiswal.prashant@gmail.com>
2020-08-31 00:21:28 +0530

    stack implemented with linked list and tes for push and pop

--- Data-Structures-Algorithms/Data-Structure-Algorithms/Stacks-Queues/Stacks.cs
+++ Data-Structures-Algorithms/Data-Structure-Algorithms/Stacks-Queues/Stacks.cs
@@ -1,10 +1,64 @@
 ﻿using System;
 namespace DataStructuresAlgorithms.StacksQueues
 {
-    public class Stacks
+    public class Stacks<T>
     {
+        public Node<T> top { get; private set; }
+        public Node<T> bottom { get; private set; }
+        public int length { get; private set; }
+
         public Stacks()
         {
+            this.top = null;
+            this.bottom = null;
+            this.length = 0;
         }
+
+        public Node<T> Peek()
+        {
+            return this.top;
+        }
+
+        public Node<T> Push(T value)
+        {
+            var node = new Node<T>(value);
+            
+            if (this.length == 0)
+                this.bottom = node;
+            node.next = this.top;
+            this.top = node;
+            this.length += 1;
+            return node;
+        }
+
+        public Node<T> Pop()
+        {
+            if(length > 0)
+            {
+                var nextNode = this.top.next;
+                var popped = this.top;
+                this.length -= 1;
+                this.top = nextNode;
+                return popped;
+            }
+            return default;
+        }
+
+        public bool IsEmpty()
+        {
+            return this.length == 0;
+        }
+    }
+
+    public class Node<T>
+    {
+        public Node(T value)
+        {
+            this.next = null;
+            this.value = value;
+        }
+
+        public Node<T> next { get; set; }
+        public T value { get; private set; }
     }
 }
---
+++ Data-Structures-Algorithms/Test-Data-Structure-Algorithms/TestStacks.cs
@@ -0,0 +1,45 @@
+﻿using DataStructuresAlgorithms.StacksQueues;
+using Xunit;
+
+namespace Test_Data_Structure_Algorithms
+{
+    public class TestStacks
+    {
+        [Fact]
+        public void TestStackPush()
+        {
+            var stack = new Stacks<string>();
+            var firstPush = stack.Push("Google");
+
+            Assert.Matches(stack.top.value, firstPush.value);
+            Assert.Matches(stack.bottom.value, firstPush.value);
+
+            var secondPush = stack.Push("Stackoverflow");
+            Assert.Matches(stack.top.value, secondPush.value);
+            Assert.True(stack.bottom.value != secondPush.value);
+
+
+            var thirdPush = stack.Push("medium blog");
+            Assert.Matches(stack.top.value, thirdPush.value);
+            Assert.True(stack.bottom.value != thirdPush.value);
+        }
+
+        [Fact]
+        public void TestStackPop()
+        {
+            var stack = new Stacks<string>();
+            var firstPush = stack.Push("Google");
+            var secondPush = stack.Push("Stackoverflow");
+            var thirdPush = stack.Push("medium blog");
+
+            var pop1 = stack.Pop();
+            Assert.True(pop1.value == "medium blog");
+
+            var pop2 = stack.Pop();
+            Assert.True(pop2.value == "Stackoverflow");
+
+            var pop3 = stack.Pop();
+            Assert.True(pop3.value == "Google");
+        }
+    }
+}
