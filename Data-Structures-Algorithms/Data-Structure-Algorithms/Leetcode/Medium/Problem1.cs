7d2a04aba1ae81f0b51274b0674956ff3c97d2ed
Prashant Jaiswal <jaiswal.prashant@gmail.com>
2020-09-02 02:11:33 +0530

    Leetcode problem

---
+++ Data-Structures-Algorithms/Data-Structure-Algorithms/Leetcode/Medium/Problem1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+namespace DataStructuresAlgorithms.Leetcode
+{
+    /// <summary>
+    /// You are given two non-empty linked lists representing two non-negative integers.
+    /// The digits are stored in reverse order and each of their nodes contain a single digit.
+    /// Add the two numbers and return it as a linked list.
+    /// You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+    /// </summary>
+    public class Problem1
+    {
+        //Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+        //Output: 7 -> 0 -> 8
+        //Explanation: 342 + 465 = 807.
+
+        public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            ListNode listNode = null;
+            ListNode currentNode = null;
+            while(l1 != null || l2 != null)
+            {
+                int sum = 0;
+                if (l1 != null)
+                {
+                    sum += l1.val;
+                    l1 = l1.next;
+                }
+                if (l2 != null)
+                {
+                    sum += l2.val;
+                    l2 = l2.next;
+                }
+
+                if(listNode == null)
+                {
+                    listNode = new ListNode(sum % 10);
+                    if (!(l1 == null && l2 == null && (sum / 10 == 0 || sum == 0)))
+                        listNode.next = new ListNode(sum / 10);
+                    currentNode = listNode.next;
+                }
+                else
+                {
+                    currentNode.val += (sum % 10);
+                    if (!(l1 == null && l2 == null && (sum / 10 == 0 || sum == 0)))
+                        currentNode.next = new ListNode(sum / 10);
+                    currentNode = currentNode.next;
+                }
+            }
+
+            return listNode;
+        }
+    }
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
---
+++ Data-Structures-Algorithms/Test-Data-Structure-Algorithms/LeetCode/Medium/TestProblem3.cs
@@ -0,0 +1,69 @@
+﻿using DataStructuresAlgorithms.Leetcode;
+using Xunit;
+
+namespace Test_Data_Structure_Algorithms.LeetCode.Medium
+{
+    public class TestProblem3
+    {
+        [Fact]
+        public void TestProblem3WithExample1()
+        {
+            //Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+            //Output: 7 -> 0 -> 8
+            //Explanation: 342 + 465 = 807.
+            var problem1 = new Problem1();
+            ListNode l1 = new ListNode(6, new ListNode(5, new ListNode(8)));
+            ListNode l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
+            //856 + 465 = 1321 -> 1231
+            var solution = problem1.AddTwoNumbers(l1, l2);
+            Assert.True(solution.val == 1);
+            Assert.True(solution.next.val == 2);
+            Assert.True(solution.next.next.val == 3);
+            Assert.True(solution.next.next.next.val == 1);
+        }
+
+        [Fact]
+        public void TestProblem3WithExample1ZeroInMiddle()
+        {
+            //Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+            //Output: 7 -> 0 -> 8
+            //Explanation: 342 + 465 = 807.
+            var problem1 = new Problem1();
+            ListNode l1 = new ListNode(1, new ListNode(0, new ListNode(1)));
+            ListNode l2 = new ListNode(2, new ListNode(0, new ListNode(2)));
+            //101 + 202 = 303 -> 303
+            var solution = problem1.AddTwoNumbers(l1, l2);
+            Assert.True(solution.val == 1);
+            Assert.True(solution.next.val == 0);
+            Assert.True(solution.next.next.val == 3);
+        }
+
+        [Fact]
+        public void TestProblem3WithExample1WithZero()
+        {
+            //Input: (0) + (0)
+            //Output: 0
+            //Explanation: 342 + 465 = 807.
+            var problem1 = new Problem1();
+            ListNode l1 = new ListNode(0);
+            ListNode l2 = new ListNode(0);
+            //856 + 465 = 1321 -> 1231
+            var solution = problem1.AddTwoNumbers(l1, l2);
+            Assert.True(solution.val == 0);
+        }
+
+        [Fact]
+        public void TestProblem3WithExample1WithSIngleDigits()
+        {
+            //Input: (0) + (0)
+            //Output: 0
+            //Explanation: 342 + 465 = 807.
+            var problem1 = new Problem1();
+            ListNode l1 = new ListNode(0);
+            ListNode l2 = new ListNode(1);
+            //856 + 465 = 1321 -> 1231
+            var solution = problem1.AddTwoNumbers(l1, l2);
+            Assert.True(solution.val == 1);
+        }
+    }
+}
