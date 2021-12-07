66734c64a095ea3c178401357dde5b94d29c5155
Prashant Jaiswal <Prashant.Jaiswal@1e.com>
2021-12-07 23:45:22 +0530

    https://leetcode.com/problems/contains-duplicate/

---
+++ Data-Structures-Algorithms/Data-Structure-Algorithms/Leetcode/Easy/Problem3.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructuresAlgorithms.Leetcode.Easy
+{
+    /// <summary>
+    /// https://leetcode.com/problems/contains-duplicate/
+    /// </summary>
+    public class Problem3
+    {
+        public bool ContainsDuplicate(int[] nums)
+        {
+            Dictionary<int, int> catalog = new Dictionary<int, int>();
+            for(int i = 0; i < nums.Length; i++)
+            {
+                int current = nums[i];
+                if (!catalog.ContainsKey(current))
+                    catalog[current] = current;
+                else
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ Data-Structures-Algorithms/Test-Data-Structure-Algorithms/LeetCode/Easy/TestProblem3.cs
@@ -0,0 +1,36 @@
+﻿using DataStructuresAlgorithms.Leetcode.Easy;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Test_Data_Structure_Algorithms.LeetCode.Easy
+{
+    public class TestProblem3
+    {
+        [Fact]
+        public void TestProblem2WithExample1()
+        {
+            Problem3 problem3 = new Problem3();
+
+            //Input: nums = [1, 2, 3, 1]
+            //Output: true
+            var nums = new int[] { 1, 2, 3, 1 };
+            var solution = problem3.ContainsDuplicate(nums);
+            Assert.True(solution);
+
+            //Input: nums = [1, 2, 3, 4]
+            //Output: false
+            nums = new int[] { 1, 2, 3, 4 };
+            var solution2 = problem3.ContainsDuplicate(nums);
+            Assert.False(solution2);
+
+            //Input: nums = [1, 1, 1, 3, 3, 4, 3, 2, 4, 2]
+            //Output: true
+            nums = new int[] { 1, 1, 1, 3, 3, 4, 3, 2, 4, 2 };
+            var solution3 = problem3.ContainsDuplicate(nums);
+            Assert.True(solution3);
+
+        }
+    }
+}
