3f01450cdae33cb9d424eaf2fdcaf80eb73d3c94
Prashant Jaiswal <Prashant.Jaiswal@1e.com>
2021-12-08 12:25:29 +0530

    https://leetcode.com/problems/maximum-subarray/

---
+++ Data-Structures-Algorithms/Data-Structure-Algorithms/Leetcode/Easy/Problem4.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructuresAlgorithms.Leetcode.Easy
+{
+    /// <summary>
+    /// https://leetcode.com/problems/maximum-subarray/
+    /// </summary>
+    public class Problem4
+    {
+        public int MaxSubArray(int[] nums)
+        {
+            int max_so_far = int.MinValue, max_here = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                max_here += nums[i];
+                if (max_here > max_so_far)
+                    max_so_far = max_here;
+
+                if (max_here < 0)
+                    max_here = 0;
+            }
+            return max_so_far;
+        }
+    }
+}
---
+++ Data-Structures-Algorithms/Test-Data-Structure-Algorithms/LeetCode/Easy/TestProblem4.cs
@@ -0,0 +1,36 @@
+﻿using DataStructuresAlgorithms.Leetcode.Easy;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Test_Data_Structure_Algorithms.LeetCode.Easy
+{
+    public class TestProblem4
+    {
+        [Fact]
+        public void TestProblem4WithExample1()
+        {
+            Problem4 problem4 = new Problem4();
+
+            //Input: nums = [1, 2, 3, 1]
+            //Output: true
+            var nums = new int[] { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
+            var solution = problem4.MaxSubArray(nums);
+            Assert.Equal(6, solution);
+
+            //Input: nums = [1, 2, 3, 4]
+            //Output: false
+            nums = new int[] { 1 };
+            var solution2 = problem4.MaxSubArray(nums);
+            Assert.Equal(1, solution2);
+
+            //Input: nums = [1, 1, 1, 3, 3, 4, 3, 2, 4, 2]
+            //Output: true
+            nums = new int[] { 5, 4, -1, 7, 8 };
+            var solution3 = problem4.MaxSubArray(nums);
+            Assert.Equal(23, solution3);
+
+        }
+    }
+}
