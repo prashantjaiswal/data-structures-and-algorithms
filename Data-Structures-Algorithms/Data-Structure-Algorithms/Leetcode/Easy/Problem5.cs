bbc1c6a7c9fbbef0741a1e9df0e5b9406233fa28
Prashant Jaiswal <Prashant.Jaiswal@1e.com>
2021-12-09 08:54:09 +0530

    indices of two numbers which sums up to a given number

---
+++ Data-Structures-Algorithms/Data-Structure-Algorithms/Leetcode/Easy/Problem5.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructuresAlgorithms.Leetcode.Easy
+{
+    /// <summary>
+    /// https://leetcode.com/problems/two-sum/
+    /// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+    /// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+    /// You can return the answer in any order.
+    /// Example 1:
+    /// Input: nums = [2, 7, 11, 15], target = 9
+    /// Output: [0,1]
+    /// Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+    /// </summary>
+    public class Problem5
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            var dictionary = new Dictionary<int, int>();
+            
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int diff = target - nums[i];
+                if (dictionary.ContainsValue(diff))
+                {
+                    dictionary.
+                    foreach (var d in dictionary)
+                    {
+                        if (d.Value == diff)
+                            return new int[] { d.Key, i };
+                    }
+                }
+                dictionary.Add(i, nums[i]);
+            }
+            return new int[] { };
+        }
+    }
+}
---
+++ Data-Structures-Algorithms/Test-Data-Structure-Algorithms/LeetCode/Easy/TestProblem5.cs
@@ -0,0 +1,40 @@
+﻿using DataStructuresAlgorithms.Leetcode.Easy;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Test_Data_Structure_Algorithms.LeetCode.Easy
+{
+    public class TestProblem5
+    {
+        [Fact]
+        public void TestProblem5WithExample1()
+        {
+            Problem5 problem5 = new Problem5();
+
+            //Input: nums = [1, 2, 3, 1]
+            //Output: true
+            var nums = new int[] { 2, 7, 11, 15 };
+            var target = 9;
+            var solution = problem5.TwoSum(nums, target);
+            Assert.Equal(new int[] { 0, 1 }, solution);
+
+            //Input: nums = [1, 2, 3, 4]
+            //Output: false
+            nums = new int[] { 3, 2, 4 };
+            target = 6;
+            var solution2 = problem5.TwoSum(nums, target);
+            Assert.Equal(new int[] { 1, 2 }, solution2);
+
+            //Input: nums = [1, 1, 1, 3, 3, 4, 3, 2, 4, 2]
+            //Output: true
+            nums = new int[] { 3, 3 };
+            target = 6;
+            var solution3 = problem5.TwoSum(nums, target);
+            Assert.Equal(new int[] { 0, 1 }, solution3);
+
+        }
+    }
+}
+
