ac8c427dd1a93ee88d8ccab60db5b6f4fcd6378d
Prashant Jaiswal <jaiswal.prashant@gmail.com>
2021-12-13 22:16:16 +0530

    recursion, factorial, power, sum, taylor series

--- Data-Structures-Algorithms/Data-Structure-Algorithms/Recursion/Recursion.cs
+++ Data-Structures-Algorithms/Data-Structure-Algorithms/Recursion/Recursion.cs
@@ -1,10 +1,39 @@
-﻿using System;
+﻿
+using System;
 namespace DataStructuresAlgorithms.Recursion
 {
     public class Recursion
     {
         public Recursion()
         {
+        }
+        public int PowerOfNImplementation1(int variable, int number)
+        {
+            if (number == 0) return 1;
+            return variable * PowerOfNImplementation1(variable, number - 1);
+        }
+
+        public int PowerOfNImplementation2(int variable, int number)
+        {
+            if (number == 0) return 1;
+
+            if (number % 2 == 0)
+            {
+                return PowerOfNImplementation2(variable * variable, number / 2);
+            }
+            return variable * PowerOfNImplementation1(variable * variable, (number - 1) / 2);
+        }
+
+        public int factorial(int n)
+        {
+            if (n == 0) return 1;
+            return n * factorial(n - 1);
+        }
+
+        public int SumOfNaturalNumber(int number)
+        {
+            if (number == 0) return 1;
+            return number + SumOfNaturalNumber(number - 1);
         }
     }
 }
