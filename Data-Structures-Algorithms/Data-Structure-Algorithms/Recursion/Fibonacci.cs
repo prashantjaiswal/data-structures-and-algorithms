9592b62e6138d2d02eadc133db3986456ddd999a
Prashant Jaiswal <jaiswal.prashant@gmail.com>
2021-12-13 23:58:20 +0530

    fibonacci with and without memoization

---
+++ Data-Structures-Algorithms/Data-Structure-Algorithms/Recursion/Fibonacci.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataStructuresAlgorithms.Recursion
+{
+    public class Fibonacci
+    {
+        public Fibonacci()
+        {
+        }
+        /// <summary>
+        /// Complexity O(2^n)
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public int Fibonacci_1(int n)
+        {
+            if (n <= 1) return n;
+            return Fibonacci_1(n - 1) + Fibonacci_1(n - 2);
+        }
+
+        Dictionary<int,int> storage = new Dictionary<int,int>();
+        /// <summary>
+        /// Fibonacci using Memoization having complexity O(n)
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public int Fibonacci_2(int n)
+        {
+            if (n <= 1)
+            {
+                storage[n] = n;
+                return n;
+            }
+            if (!storage.ContainsKey(n - 1))
+                storage[n - 1] = Fibonacci_2(n - 1);
+            if (!storage.ContainsKey(n - 2))
+                storage[n - 2] = Fibonacci_2(n - 2);
+            return storage[n - 1] + storage[n - 2];
+        }
+    }
+}
--- Data-Structures-Algorithms/Data-Structure-Algorithms/Recursion/TaylorSeries.cs
+++ Data-Structures-Algorithms/Data-Structure-Algorithms/Recursion/TaylorSeries.cs
@@ -6,14 +6,30 @@ namespace DataStructuresAlgorithms.Recursion
     /// </summary>
     public class TaylorSeries
     {
-        public int TaylorSeriesImp1(int x, int n)
+        public double TaylorSeriesImp1(int x, int n)
         {
-            int power = 0, factorial = 0, returnValue = 0;
+            int power = 0, factorial = 0;
+            double returnValue = 0;
             if (n == 0) return 1;
             returnValue = TaylorSeriesImp1(x, n - 1);
             power *= x;
             factorial *= factorial;
             return returnValue + power / factorial;
+        }
+
+
+        private double storage = 1;
+        /// <summary>
+        /// Taylor series using Horner's rule
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public double TaylorSeriesImp2(int x, int n)
+        {
+            if (n == 0) return storage;
+            storage = 1 + (x / n) * storage;
+            return TaylorSeriesImp2(x, n - 1);
         }
     }
 }
