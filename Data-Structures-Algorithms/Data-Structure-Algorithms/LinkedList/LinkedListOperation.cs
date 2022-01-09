3e7f661c54c7ab76820ce46da6aa34344b69bd25
Prashant Jaiswal <Prashant.Jaiswal@1e.com>
2022-01-09 16:41:22 +0530

    Reverse a given number

---
+++ Data-Structures-Algorithms/Data-Structure-Algorithms/LinkedList/LinkedListOperation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DataStructuresAlgorithms.LinkedList.Structure;
+
+namespace DataStructuresAlgorithms.LinkedList
+{
+    public class LinkedListOperation
+    {
+        public void Reverse(LinkedList<int> list)
+        {
+            Node<int> previous = new Node<int>(0);
+            Node<int> next = list.head;
+            Node<int> current = new Node<int>(0);
+            //list.tail = next;
+            while (next.next != null)
+            {
+                previous = current;
+                current = next;
+                next = next.next;
+
+            }
+        }
+    }
+}
