dc0264b2804382bc7e1c69e9ff27511baef560a2
Prashant Jaiswal <jaiswal.prashant@gmail.com>
2022-01-19 23:24:16 +0530

    problem 3 finding substring with no repeating char

--- Data-Structures-Algorithms/Data-Structure-Algorithms/Recursion/TowerOfHanoi.cs
+++ Data-Structures-Algorithms/Data-Structure-Algorithms/Recursion/TowerOfHanoi.cs
@@ -3,13 +3,13 @@ namespace DataStructuresAlgorithms.Recursion
 {
     public class TowerOfHanoi
     {
-        public void TowerOfHanoiOperation(int numberOfPlates, int towerA, int towerB, int towerC)
+        public void TowerOfHanoiSolution(int numberOfPlates, int towerA, int towerB, int towerC)
         {
             if(numberOfPlates > 0)
             {
-                TowerOfHanoiOperation(numberOfPlates - 1, towerA, towerC, towerB);
+                TowerOfHanoiSolution(numberOfPlates - 1, towerA, towerC, towerB);
                 Console.WriteLine($"Move from: {towerA} to {towerC}");
-                TowerOfHanoiOperation(numberOfPlates - 1, towerB, towerA, towerC);
+                TowerOfHanoiSolution(numberOfPlates - 1, towerB, towerA, towerC);
             }
         }
     }
