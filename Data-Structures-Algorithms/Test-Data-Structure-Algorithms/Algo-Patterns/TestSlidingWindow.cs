21509e6bd9ca52155cc2507a6e4b8ae397738255
Prashant Jaiswal <Prashant.Jaiswal@1e.com>
2022-01-31 19:56:47 +0530

    Count max fruots in two buckets

--- Data-Structures-Algorithms/Test-Data-Structure-Algorithms/Algo-Patterns/TestSlidingWindow.cs
+++ Data-Structures-Algorithms/Test-Data-Structure-Algorithms/Algo-Patterns/TestSlidingWindow.cs
@@ -11,7 +11,7 @@ namespace Test_Data_Structure_Algorithms.AlgoPatterns
         {
             Problem01 problem01 = new Problem01();
             int[] param = new int[] { 2, 1, 5, 1, 3, 2 };
-            int max = problem01.MaxSubArraySum(param,3);
+            int max = problem01.MaxSubArraySum(param, 3);
             Assert.Equal(9, max);
 
             param = new int[] { 2, 3, 4, 1, 5 };
@@ -28,7 +28,7 @@ namespace Test_Data_Structure_Algorithms.AlgoPatterns
             Assert.Equal(2, max);
 
             param = new int[] { 2, 1, 5, 2, 8 };
-            max = problem01.FindMinSubArray( 2, param);
+            max = problem01.FindMinSubArray(2, param);
             Assert.Equal(1, max);
         }
 
@@ -47,6 +47,17 @@ namespace Test_Data_Structure_Algorithms.AlgoPatterns
 
             max = problem.FindLength("cbbebi", 10);
             Assert.Equal(6, max);
+        }
+
+        [Fact]
+        public void TestFindFruitLength()
+        {
+            Problem04 problem = new Problem04();
+            int max = problem.FindLength(new[] { 'A', 'B', 'C', 'A', 'C' });
+            Assert.Equal(3, max);
+
+            max = problem.FindLength(new []{'A', 'B', 'C', 'B', 'B', 'C'});
+            Assert.Equal(5, max);
         }
     }
 }
