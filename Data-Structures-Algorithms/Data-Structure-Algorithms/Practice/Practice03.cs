752018f01b1216359d82518c52af53594a6f57ee
Prashant Jaiswal <jaiswal.prashant@gmail.com>
2022-02-25 10:13:15 +0530

    Pre sum problem

--- Data-Structures-Algorithms/Data-Structure-Algorithms/Practice/Practice03.cs
+++ Data-Structures-Algorithms/Data-Structure-Algorithms/Practice/Practice03.cs
@@ -43,7 +43,139 @@ namespace DataStructuresAlgorithms.Practice
          */
         public int SumOfOddEvenByRemovingOneElement(List<int> A)
         {
-            throw new NotImplementedException();
+            int count = A.Count;
+            var listOdd = new List<int>();
+            var listEven = new List<int>();
+            int indexCount = 0;
+
+            for(int j = 0; j< count; j++)
+            {
+                if(j%2 == 0)
+                {
+                    listEven.Add(A[j]);
+                    listOdd.Add(0);
+                }
+                else
+                {
+                    listOdd.Add(A[j]);
+                    listEven.Add(0);
+                }
+            }
+
+            for (int i = 0; i < count - 1; i++)
+            {
+                listOdd[i + 1] += listOdd[i];
+                listEven[i + 1] += listEven[i];
+            }
+
+            for(int i = 0; i < count; i++)
+            {
+                if (i == 0 &&
+                    listOdd[count - 1] - listOdd[i] == listEven[count - 1] - listEven[i])
+                {
+                    indexCount++;
+                }
+                else if (i != 0 && listEven[i - 1] + listOdd[count - 1] - listOdd[i] ==
+                    listOdd[i - 1] + listEven[count - 1] - listEven[i])
+                {
+                    indexCount++;
+                }
+            }
+            return indexCount;
+        }
+
+        /*
+        You are given an integer array A of length N.
+        You are also given a 2D integer array B with dimensions M x 2, where each row denotes a [L, R] query.
+        For each query, you have to find the sum of all elements from L to R indices in A (1 - indexed).
+        More formally, find A[L] + A[L + 1] + A[L + 2] +... + A[R - 1] + A[R] for each query.
+
+
+
+        Problem Constraints
+        1 <= N, M <= 105
+        1 <= A[i] <= 109
+        1 <= L <= R <= N
+         */
+        public List<long> RangeSum(List<int> A, List<List<int>> B)
+        {
+            int n = A.Count;
+            var listA = new List<long>();
+            for (int i = 0; i < n; i++)
+            {
+                if (i == 0) listA.Add(A[i]);
+                else listA.Add(A[i] + listA[i-1]);
+            }
+            var list = new List<long>();
+            for (int i = 0; i < B.Count; i++)
+            {
+                int L = B[i][0] -1;
+                int R = B[i][1] -1;
+                if (L > 0)
+                    list.Add(listA[R] - listA[L - 1]);
+                else
+                    list.Add(listA[R]);
+            }
+            return list;
+        }
+
+        /*
+        Given an integer array A of size N.
+        You can pick B elements from either left or right end of the array A to get maximum sum.
+        Find and return this maximum possible sum.
+        NOTE: Suppose B = 4 and array A contains 10 elements then
+        You can pick first four elements or can pick last four elements or can pick 1 from
+        front and 3 from back etc . you need to return the maximum possible sum of elements you can pick.
+        Problem Constraints
+        1 <= N <= 105
+        1 <= B <= N
+        -103 <= A[i] <= 103
+         */
+        //[5, -2, 3 , 1, 2] -> [5, 3, 6, 7, 8] -> rotate 7 8 5 3 6
+       
+        public int MaximumPossibleSum(List<int> A, int B)
+        {
+            int max = int.MinValue;
+            int n = A.Count;
+            var frontArr = new int[B +1 ];
+            var rearArr = new int[B + 1];
+
+            for (int i = 0; i < B; i++)
+            {
+                frontArr[i + 1] = A[i] + frontArr[i];
+                rearArr[i + 1] = A[n - i - 1] + rearArr[i];
+            }
+
+            for (int i = 0; i <= B; i++)
+            {
+                int m = frontArr[i] + rearArr[B - i];
+                max = Math.Max(max, m);
+            }
+            return max;
+        }
+
+        /*
+         You are given an integer array A. You have to find the second
+        largest element/value in the array or report that no such element exists.
+         */
+        public int SecondLargest(List<int> A)
+        {
+            int first = int.MinValue;
+            int second = int.MinValue;
+            int current = 0;
+            int third = int.MinValue;
+            if (A.Count <= 1) return -1;
+
+            for (int i = 0; i < A.Count; i++)
+            {
+                current = A[i];
+                if (current > first) first = current;
+                if (current > second && current < first) second = current;
+                else third = current;
+            }
+            if(third > int.MinValue)
+                return second;
+            return -1;
         }
     }
 }
--- Data-Structures-Algorithms/Test-Data-Structure-Algorithms/Practice/TestPractice03.cs
+++ Data-Structures-Algorithms/Test-Data-Structure-Algorithms/Practice/TestPractice03.cs
@@ -20,5 +20,51 @@ namespace Test_Data_Structure_Algorithms.Practice
             result = practice.EquilibriumIndex(new List<int> { 0, 0, 0 });
             Assert.Equal(0, result);
         }
+
+        [Fact]
+        public void TestSumOfOddEvenByRemovingOneElement()
+        {
+            Practice03 practice = new Practice03();
+            int result = practice.SumOfOddEvenByRemovingOneElement(new List<int> { 2, 1, 6, 4 });
+            Assert.Equal(1, result);
+
+            result = practice.SumOfOddEvenByRemovingOneElement(new List<int> { 1, 1, 1 });
+            Assert.Equal(3, result);
+        }
+
+        [Fact]
+        public void TestRangeSum()
+        {
+            Practice03 practice = new Practice03();
+            
+            var result = practice.RangeSum(new List<int> { 1, 2, 3, 4, 5 }, new List<List<int>> { new List<int> { 1, 4 }, new List<int> { 2, 3 } });
+            //[10, 5]
+            result = practice.RangeSum(new List<int> { 2,2,2 }, new List<List<int>> { new List<int> { 1, 1 }, new List<int> { 2, 3 } });
+            //[2,4]
+        }
+        [Fact]
+        public void TestMaximumPossibleSum()
+        {
+            Practice03 practice = new Practice03();
+            int result = practice.MaximumPossibleSum(new List<int> { 5, -2, 3, 1, 2 }, 3);
+            Assert.Equal(8, result);
+        
+            result = practice.MaximumPossibleSum(new List<int> { 1, 2 }, 1);
+            Assert.Equal(2, result);
+        }
+
+        [Fact]
+        public void TestSecondLargest()
+        {
+            Practice03 practice = new Practice03();
+            int result = practice.SecondLargest(new List<int> { 2, 1, 2});
+            Assert.Equal(2, result);
+
+            result = practice.SecondLargest(new List<int> { 2 });
+            Assert.Equal(-1, result);
+
+            result = practice.SecondLargest(new List<int> { 5, -2, 3, 1, 2 });
+            Assert.Equal(3, result);
+        }
     }
 }
