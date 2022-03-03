da19b4bd61cca49d0fb5314cc6f967eaa5f52ec3
Prashant Jaiswal <jaiswal.prashant@gmail.com>
2022-03-03 09:46:20 +0530

    Matrices

---
+++ Data-Structures-Algorithms/Data-Structure-Algorithms/Practice/Practice06.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataStructuresAlgorithms.Practice
+{
+    public class Practice06
+    {
+        /*
+        You are given two matrices A & B of same size, you have to return
+        another matrix which is the sum of A and B.
+         */
+        public List<List<int>> SumOfTwoMatrices(List<List<int>> A, List<List<int>> B)
+        {
+            if (A.Count <= 0 && A[0].Count <= 0) return null;
+            int rows = A.Count;
+            int cols = A[0].Count;
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    A[i][j] = A[i][j] + B[i][j];
+                }
+            }
+            return A;
+        }
+    }
+}
---
+++ Data-Structures-Algorithms/Test-Data-Structure-Algorithms/Practice/TestPractice06.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using DataStructuresAlgorithms.Practice;
+using Xunit;
+
+namespace Test_Data_Structure_Algorithms.Practice
+{
+    public class TestPractice06
+    {
+
+        [Fact]
+        public void TestFindSubarraysEvenOdd()
+        {
+            Practice06 practice = new Practice06();//
+            //
+            var result = practice.SumOfTwoMatrices(new List<List<int>> {new List<int> { 1, 2, 3 }, new List<int> { 4, 5, 6 }, new List<int> {7,8,9 } },
+                new List<List<int>> { new List<int> { 9,8,7 }, new List<int> {6,5,4 }, new List<int> { 3,2,1 } });
+            //Assert.Equal(6, result);
+        }
+    }
+}
