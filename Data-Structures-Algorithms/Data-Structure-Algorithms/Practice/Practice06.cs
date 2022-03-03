afc2f6d71af8e248344375f824efe5003237de28
Prashant Jaiswal <Prashant.Jaiswal@1e.com>
2022-03-03 21:26:08 +0530

    Matrix

--- Data-Structures-Algorithms/Data-Structure-Algorithms/Practice/Practice06.cs
+++ Data-Structures-Algorithms/Data-Structure-Algorithms/Practice/Practice06.cs
@@ -23,5 +23,78 @@ namespace DataStructuresAlgorithms.Practice
             }
             return A;
         }
+        /*
+         You are given a 2D integer matrix A, return a 1D integer vector containing column-wise sums of original matrix.
+         */
+        public List<int> SumRowWiseMatrix(List<List<int>> A)
+        {
+            if (A.Count <= 0 && A[0].Count <= 0) return null;
+            var sumMatrix = new List<int>();
+            for (int i = 0; i < A[0].Count; i++)
+            {
+                int sum = 0;
+                for (int j = 0; j < A.Count; j++)
+                {
+                    sum += A[j][i];
+                }
+                sumMatrix.Add(sum);
+            }
+
+            return sumMatrix;
+        }
+
+        /*
+         Give a N * N square matrix A, return an array of its anti-diagonals. Look at the example for more details.
+            1 2 3
+            4 5 6
+            7 8 9
+         */
+        public List<List<int>> AntiDiagonalArray(List<List<int>> A)
+        {
+            var array = new List<List<int>>();
+            int n = A.Count;
+            for (int i = n; i > 0; i--)
+            {
+                int sum = 0;
+                for (int j = 0; j <= n - i; j++)
+                {
+                    sum += A[i][j];
+                }
+            }
+
+            return array;
+        }
+
+
+        /*
+         You are given two integer matrices A(having M X N size) and B(having N X P). 
+         You have to multiply matrix A with B and return the resultant matrix. (i.e. return the matrix AB).
+         */
+        public List<List<int>> MultiplyTwoMatrices(List<List<int>> A, List<List<int>> B)
+        {
+            if ((A.Count <= 0 && A[0].Count <= 0) || B.Count <= 0 && B[0].Count <= 0) return null;
+            // A = M X N, B = N X P
+            var multipliedMatrix = new List<List<int>>();
+            int cols = A.Count;
+            int rows = B[0].Count;
+            int n = B.Count;
+            for (int i = 0; i < rows; i++)
+            {
+                var row = new List<int>();
+                int sum = 0;
+                for (int j = 0; j < cols; j++)
+                {
+                    for (int k = 0; k < n; k++)
+                    {
+                        sum += A[i][k] * B[k][j];
+                    }
+
+                    row.Add(sum);
+                }
+                multipliedMatrix.Add(row);
+            }
+
+            return multipliedMatrix;
+        }
     }
 }
--- Data-Structures-Algorithms/Test-Data-Structure-Algorithms/Practice/TestPractice06.cs
+++ Data-Structures-Algorithms/Test-Data-Structure-Algorithms/Practice/TestPractice06.cs
@@ -7,7 +7,6 @@ namespace Test_Data_Structure_Algorithms.Practice
 {
     public class TestPractice06
     {
-
         [Fact]
         public void TestFindSubarraysEvenOdd()
         {
@@ -16,6 +15,27 @@ namespace Test_Data_Structure_Algorithms.Practice
             var result = practice.SumOfTwoMatrices(new List<List<int>> {new List<int> { 1, 2, 3 }, new List<int> { 4, 5, 6 }, new List<int> {7,8,9 } },
                 new List<List<int>> { new List<int> { 9,8,7 }, new List<int> {6,5,4 }, new List<int> { 3,2,1 } });
             //Assert.Equal(6, result);
+        }
+
+        [Fact]
+        public void TestMultiplyTwoMatrices()
+        {
+            Practice06 practice = new Practice06();//
+            //
+            var result = practice.MultiplyTwoMatrices(new List<List<int>> { new List<int> { 1, 2 }, new List<int> { 3,4} },
+                new List<List<int>> { new List<int> { 5, 6 }, new List<int> { 7 ,8 } });
+
+            result = practice.MultiplyTwoMatrices(new List<List<int>> { new List<int> { 1, 1 } },
+                new List<List<int>> { new List<int> { 2 }, new List<int> { 3 } });
+            //Assert.Equal(6, result);
+        }
+
+        [Fact]
+        public void TestSumRowWiseMatrix()
+        {
+            Practice06 practice = new Practice06();//
+            //
+            var result = practice.SumRowWiseMatrix(new List<List<int>> { new List<int> { 1, 2, 3, 4 }, new List<int> { 5, 6, 7, 8 }, new List<int>{ 9, 2, 3, 4 } });
         }
     }
 }
